11132a803740cd3f440a1e97b74dce63a0725963
Wika <127410155+FloryneH@users.noreply.github.com>
2023-07-25 11:42:19 +0200

    feat/main/01 : code php pour le premier pdf d'exercices

---
+++ src/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Exercice 1</h2>
+    <?php
+        $nom = "Hamouda";
+        echo $nom;
+    ?>
+
+    <h2>Exercice 2</h2>
+    <?php
+        $nom = "Hamouda";
+        echo $nom;
+        echo "<br>";
+        $prenom = "Floryne";
+        echo $prenom;
+        echo "<br>";
+        $age = 20;
+        echo $age;
+    ?>
+
+    <h2>Exercice 3</h2>
+    <?php
+        $km = 1 ;
+        echo $km;
+        echo "<br>";
+        $km = 3 ;
+        echo $km;
+        echo "<br>";
+        $km = 125;
+        echo $km;
+    ?>
+
+    <h2>Exercice 4</h2>
+    <?php
+        $string = "Je m'appelle Floryne";
+        echo $string;
+        echo "<br>";
+        $int = 20;
+        echo $int;
+        echo "<br>";
+        $float = 20.5;
+        echo $float;
+        echo "<br>";
+        $boolean =  true;
+        echo $boolean;
+    ?>
+
+    <h2>Exercice 5</h2>
+    <?php
+        $int2;
+        echo $int2;
+        echo "<br>";
+        $int2 = 30;
+        echo $int2;
+    ?>
+
+    <h2>Exercice 6</h2>
+    <?php
+        $nom2 = "Jessica";
+        echo  "Bonjour " .$nom2. " comment vas tu ?"
+    ?>
+
+    <h2>Exercice 7</h2>
+    <?php
+        $nom;
+        $prenom;
+        $age;
+        echo "bonjour " .$nom. " " .$prenom. " tu as " .$age. " ans"
+    ?>
+
+    <h2>Exercice 8</h2>
+    <?php
+        $a = 3 + 4;
+        echo $a;
+        echo "<br>";
+        $b = 5 * 20;
+        echo $b;
+        echo "<br>";
+        $c = 45 / 5;
+        echo $c;
+    ?>
+
+</body>
+</html>
