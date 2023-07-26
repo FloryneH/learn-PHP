f5ae80aec990f9f1795d913318635b7463d67f02
Wika <127410155+FloryneH@users.noreply.github.com>
2023-07-26 16:55:39 +0200

    feat/main/06.1 : début du code php pour le sixième  pdf d'exercices

---
+++ src/sessionF/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Exercic 1</h2>
+    <?php
+        echo date('d/m/Y');
+    ?>
+
+    <h2>Exercic 2</h2>
+    <?php
+        echo date('d/m/Y');
+    ?>
+
+    <h2>Exercic 3</h2>
+    <?php
+       
+    ?>
+</body>
+</html>
